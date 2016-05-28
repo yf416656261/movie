3337f1bb538313a03a00dafc6f1a86ed89af67ba
13331326-yuanyue <1062856624@qq.com>
2016-05-28 23:42:56 +0800

    登录及用户信息界面

---
+++ src/com/example/movie/User.java
@@ -0,0 +1,21 @@
+package com.example.yy.movie;
+
+/**
+ * Created by yy on 2016/5/28.
+ */
+public class User {
+    private int id;
+    private String password;
+    private String name;
+    private int picture_id;
+
+    public void setId(int a) {id = a;}
+    public void setPassword(String a) {password = a;}
+    public void setName(String a) {name = a;}
+    public void setPicture_id(int a) {picture_id = a;}
+
+    public int getId() {return id;}
+    public int getPicture_id() {return picture_id;}
+    public String getPassword() {return password;}
+    public String getName() {return name;}
+}
