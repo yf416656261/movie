c07a99f05f8ae40d85fd07a38d2799f88a6925a3
13331326-yuanyue <1062856624@qq.com>
2016-05-29 16:16:32 +0800

    用户信息界面的填充

--- src/com/example/movie/User.java
+++ src/com/example/movie/User.java
@@ -1,5 +1,7 @@
 package com.example.movie;
 
+import java.lang.String;
+
 /**
  * Created by yy on 2016/5/28.
  */
@@ -8,14 +10,26 @@ public class User {
     private String password;
     private String name;
     private int picture_id;
+    private String sex;
+    private String phone;
+    private String email_address;
+    private String address;
 
     public void setId(int a) {id = a;}
     public void setPassword(String a) {password = a;}
     public void setName(String a) {name = a;}
     public void setPicture_id(int a) {picture_id = a;}
+    public void setSex(String a){sex = a;}
+    public void setPhone(String a){phone = a;}
+    public void setEmail_address(String a){email_address = a;}
+    public void setAddress(String a){address = a;}
 
     public int getId() {return id;}
     public int getPicture_id() {return picture_id;}
     public String getPassword() {return password;}
     public String getName() {return name;}
+    public String getSex() {return sex;}
+    public String getPhone() {return phone;}
+    public String getEmail_address() {return email_address;}
+    public String getAddress() {return address;}
 }
--- src/com/example/movie/User_info.java
+++ src/com/example/movie/User_info.java
@@ -6,9 +6,19 @@ import com.example.movie.Login;
 
 public class User_info extends Activity {
 
+    private TextView name = (TextView)findViewById(R.id.name);
+    private TextView sex = (TextView)findViewById(R.id.sex);
+    private TextView phone = (TextView)findViewById(R.id.phone);
+    private TextView email_address = (TextView)findViewById(R.id.email_address);
+    private TextView address = (TextView)findViewById(R.id.address);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        name.setText(Login.user.getName());
+        sex.setText(Login.user.getSex());
+        phone.setText(Login.user.getPhone());
+        email_address.setText(Login.user.getEmail_address());
+        address.setText(Login.user.getAddress());
     }
 }
